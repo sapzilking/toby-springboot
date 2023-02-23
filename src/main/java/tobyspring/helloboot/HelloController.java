9a239c3ee01aff57bd89f766cd6c25b513d5d43b
Sugil Kim <sapzilking@gmail.com>
2023-02-23 19:46:10 +0900

    스프링을 사용하지 않고 서블릿 기술만을 사용해서 frontcontroller 만들기

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,13 +1,7 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
 
-    @GetMapping("/hello")
     public String hello(String name) {
         return "Hello " + name;
     }
