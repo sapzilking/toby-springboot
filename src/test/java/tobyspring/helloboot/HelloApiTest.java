0a94d64a73ae38f204fe6b8cffa3b6d6e21db106
Sugil Kim <sapzilking@gmail.com>
2023-02-26 18:11:24 +0900

    Junit을 이용한 테스트코드 작성

--- src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -1,14 +1,11 @@
 package tobyspring.helloboot;
 
-import jdk.jfr.ContentType;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,5 +25,15 @@ public class HelloApiTest {
         assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         // body Hello Spring
         assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+
+    @Test
+    void failsHelloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
